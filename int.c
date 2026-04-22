beb96d928ffe97519042af2efd2eb4a935e7c8ae
guhuihe524513 <13880673248@163.com>
2026-04-22 09:48:46 +0800

    add_some_sourse

---
+++ int.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "int.h"
+
+int add(){
+
+    int a = 10;
+    int b = 20;
+
+    int c = a + b;
+
+    printf("the result is %d \n", c);
+
+    return 0;
+}
+
---
+++ int.h
@@ -0,0 +1,7 @@
+#ifndef _INT_H
+#define _INT_H
+
+int add();
+
+#endif // !_INT_H
+
